5e4136a832c3f7eca65c1c9d1a3bbdc8450d639a
musikr <ma-ruba@mail.ru>
2019-10-15 15:59:54 +0300

    only architecture of the future app created

---
+++ toDoList/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  toDoList
+//
+//  Created by Мария on 15.10.2019.
+//  Copyright © 2019 Мария. All rights reserved.
+//
+
+//Это паттерн проектирования MVC
+
+//вся логика приложения: сохранение данных, подгрузка данных, создание новых toDoItemов
+
+import Foundation
