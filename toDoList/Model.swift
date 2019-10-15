e7da24c9e2e90f7797213d13b3e8c7b23379b2bd
musikr <ma-ruba@mail.ru>
2019-10-15 19:05:51 +0300

    in progress

--- toDoList/Model.swift
+++ toDoList/Model.swift
@@ -11,3 +11,30 @@
 //вся логика приложения: сохранение данных, подгрузка данных, создание новых toDoItemов
 
 import Foundation
+
+var toDoItems: [[String: Any]] {
+    set {
+        UserDefaults.standard.set(newValue, forKey: "toDoDataKey")
+        UserDefaults.standard.synchronize()
+    }
+    get {
+        if let array = UserDefaults.standard.array(forKey: "toDoDataKey") as? [[String: Any]] {
+            return array
+        } else {
+           return []
+        }
+    }
+}
+
+func addItem(nameItem: String, isCompleated: Bool = false) {
+    toDoItems.append(["Name" : nameItem, "Is compleated" : isCompleated])
+}
+
+func removeItem(at index: Int) {
+    toDoItems.remove(at: index)
+}
+
+func changeStatus(at index: Int) -> Bool {
+    toDoItems[index]["Is compleated"] = !(toDoItems[index]["Is compleated"] as! Bool)
+    return toDoItems[index]["Is compleated"] as! Bool
+}
--- toDoList/TableViewController.swift
+++ toDoList/TableViewController.swift
@@ -12,6 +12,25 @@ import UIKit
 
 class TableViewController: UITableViewController {
 
+    
+    @IBAction func pushAddAction(_ sender: Any) {
+        let alertController = UIAlertController(title: "Create new item", message: nil, preferredStyle: .alert)
+        alertController.addTextField { (textField) in
+            textField.placeholder = "New Item" //значение по умолчанию
+        }
+        let alertAction1 = UIAlertAction(title: "Cancel", style: .default) { (alert) in
+            
+        }
+        let alertAction2 = UIAlertAction(title: "Create", style: .default) { (alert) in
+            let newItem = alertController.textFields![0] .text
+            addItem(nameItem: newItem!)
+            self.tableView.reloadData()
+        }
+        alertController.addAction(alertAction1)
+        alertController.addAction(alertAction2)
+        present(alertController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,43 +50,63 @@ class TableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return toDoItems.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
         // Configure the cell...
-
+        //Выводим данные из словаря
+        let dictItem = toDoItems[indexPath.row]
+        if let strItem = dictItem["Name:"] as? String {
+            cell.textLabel?.text = strItem
+        }
+        
+        //добавление галочки завершенности действия
+        if (dictItem["Is compleated"] as? Bool) == true {
+            cell.accessoryType = .checkmark
+        } else {
+             cell.accessoryType = .none
+        }
         return cell
     }
-    */
 
-    /*
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if changeStatus(at: indexPath.row) {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        
+        tableView.reloadData()
+    }
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            removeItem(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+
 
     /*
     // Override to support rearranging the table view.
