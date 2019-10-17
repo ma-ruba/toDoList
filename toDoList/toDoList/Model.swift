ed2532c7f1589261ce1b423f0d819acc0b2d7e2e
musikr <ma-ruba@mail.ru>
2019-10-17 19:12:56 +0300

    This is a first draft but the main features are ready

--- toDoList/toDoList/Model.swift
+++ toDoList/toDoList/Model.swift
@@ -8,13 +8,30 @@
 
 import Foundation
 
-var toDoItem: [String] = ["Do HW", "Call mammy"]
-
-func addItem(_ name: String)  {
-    toDoItem.append(name)
+var toDoItem: [[String: Any]] {
+    set { //вызывается при попытке изменения 
+        UserDefaults.standard.set(newValue, forKey: "toDoKey")
+        UserDefaults.standard.synchronize()
+    }
+    get { //вызывается при обращении
+        if let array = UserDefaults.standard.array(forKey: "toDoKey") as? [[String: Any]] {
+            return array
+        } else {
+            return []
+        }
+    }
+    
+}
+func addItem(_ name: String, isDone: Bool = false)  {
+    
+    toDoItem.append(["Name" : name as Any, "Is done" : isDone as Any])
 }
 
 func removeItem(at index: Int) {
     toDoItem.remove(at: index)
 }
 
+func changeStatus(at index: Int) -> Bool {
+    toDoItem[index]["Is done"] = !(toDoItem[index]["Is done"] as! Bool)
+    return toDoItem[index]["Is done"] as! Bool
+}
--- toDoList/toDoList/TableViewController.swift
+++ toDoList/toDoList/TableViewController.swift
@@ -12,8 +12,22 @@ class TableViewController: UITableViewController {
 
     
     @IBAction func addNewItem(_ sender: Any) {
-        addItem("New Item")
-        tableView.reloadData()
+       //элемент, при помощи которого выводятся стандартные всплывающие окна
+        let alertController = UIAlertController(title: "Add Item", message: "", preferredStyle: .alert)
+        alertController.addTextField { (textField) in
+            textField.placeholder = "New Item"
+        }
+        let alertAction1 = UIAlertAction(title: "Create", style: .cancel) { (action) in
+            let name = alertController.textFields![0].text
+            addItem(name!)
+            self.tableView.reloadData()
+        }
+        let alertAction2 = UIAlertAction(title: "Cancel", style: .default) { (action) in
+            
+        }
+        alertController.addAction(alertAction1)
+        alertController.addAction(alertAction2)
+        present(alertController, animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
@@ -47,8 +61,14 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        cell.textLabel?.text = toDoItem[indexPath.row]
+        let dict = toDoItem[indexPath.row]
+        cell.textLabel?.text = dict["Name"] as? String
         // Configure the cell...
+        if dict["Is done"] as! Bool == true {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
 
         return cell
     }
@@ -73,6 +93,14 @@ class TableViewController: UITableViewController {
         }    
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true) //снимаем выделение с ячейки
+        if changeStatus(at: indexPath.row) == true {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+    }
 
     /*
     // Override to support rearranging the table view.
